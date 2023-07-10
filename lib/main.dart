6c089bc018c0c670a2b5f80e8f30f4d4427c5a02
Nababkhan007 <markohenry46@gmail.com>
2023-07-11 02:34:25 +0600

    Add image preview functionality with withData and withReadStream data where true

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const WebImageTest());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WebImageTest extends StatelessWidget {
+  const WebImageTest({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // hide the debug banner
       debugShowCheckedModeBanner: false,
-      title: 'KindaCode.com',
+      title: "Web Image Test",
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -31,26 +29,22 @@ class FilePickerExample extends StatefulWidget {
 }
 
 class _FilePickerExampleState extends State<FilePickerExample> {
-  // Variable to hold the selected image file
   PlatformFile? _imageFile;
 
-  // Method to pick and display an image file
   Future<void> _pickImage() async {
     try {
-      // Pick an image file using file_picker package
       FilePickerResult? result = await FilePicker.platform.pickFiles(
+        withData: true,
+        withReadStream: true,
         type: FileType.image,
       );
 
-      // If user cancels the picker, do nothing
       if (result == null) return;
 
-      // If user picks an image, update the state with the new image file
       setState(() {
         _imageFile = result.files.first;
       });
     } catch (e) {
-      // If there is an error, show a snackbar with the error message
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(e.toString()),
@@ -63,24 +57,38 @@ class _FilePickerExampleState extends State<FilePickerExample> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('KindaCode.com'),
+        title: const Text(
+          "Web Image Test",
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // If image file is not null, display it using Image widget
             if (_imageFile != null)
-              Image.memory(
-                Uint8List.fromList(_imageFile!.bytes!),
-                width: 300,
-                height: 300,
-                fit: BoxFit.cover,
+              FutureBuilder(
+                future: _imageFile!.readStream!.first,
+                builder:
+                    (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  if (snapshot.hasData) {
+                    return Image.memory(
+                      snapshot.data,
+                      width: 300,
+                      height: 300,
+                      fit: BoxFit.cover,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
               ),
-            // A button to trigger the file picker dialog
             ElevatedButton(
               onPressed: _pickImage,
-              child: const Text('Pick an image'),
+              child: const Text(
+                "Pick an image",
+              ),
             ),
           ],
         ),
